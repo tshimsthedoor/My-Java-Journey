48c9f2a9d8f96119284c113f5a97c6dc17f81137
tshimsthedoor <tshims79@gmail.com>
2023-10-12 21:26:53 +0200

    added some output

--- src/com/thedorcode/Main.java
+++ src/com/thedorcode/Main.java
@@ -3,6 +3,11 @@ package com.thedorcode;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hello ida");
+        System.out.println("This is text");
+        System.out.println('A');
+        System.out.println(53);
+        System.out.println(23.08f);
+        System.out.println(1.97);
+        System.out.println(true);
     }
 }

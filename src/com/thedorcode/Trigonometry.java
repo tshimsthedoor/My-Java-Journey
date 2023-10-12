57dc4fce4417f174e5fff6676526da4ee6891ca6
tshimsthedoor <tshims79@gmail.com>
2023-10-12 22:03:28 +0200

    added comments for help to understand

--- src/com/thedorcode/Trigonometry.java
+++ src/com/thedorcode/Trigonometry.java
@@ -2,10 +2,14 @@ package com.thedorcode;
 
 public class Trigonometry {
     public static void main(String[] args){
-        double a = 3;
-        double b = 4;
-        double h = 0;
+        double a = 3; // first side of the triangle
+        double b = 4; // second side of the triangle
+        double h = 0; // hypotenuse, init with value 0
+
+        // equation to solve the hypotenuse
         h = Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
+
+        // print ot the results
         System.out.println(h);
     }
 }
